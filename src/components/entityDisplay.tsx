929006ce53589fb1c89191b61e34596c0f79a709
TimTwigg <tim@twiggusa.com>
2024-09-21 11:48:13 -0700

    [UDPATE] Work on EntityDisplay Controls

--- src/components/entityDisplay.tsx
+++ src/components/entityDisplay.tsx
@@ -1,44 +1,142 @@
 import { Entity } from "@src/models/entity"
 import { Card } from "@src/components/card"
-import HostileIcon from "@src/assets/crossedSwords.png"
 import * as React from "react"
+
+import { GiCrossedSwords, GiCog, GiHalfHeart, GiShield, GiChalkOutlineMurder, GiPencil, GiSpellBook, GiRun, GiMountainClimbing, GiDigDug, GiLibertyWing } from "react-icons/gi"
+import { FaSwimmer, FaAddressCard } from "react-icons/fa"
+import { IoMdHourglass } from "react-icons/io"
 
 type EntityDisplayProps = {
     entity: Entity,
     expanded?: boolean
 };
 
-function renderSpeed(speed: Entity["StatBlock"]["Stats"]["Speed"]): string {
-    return Object.entries(speed).map(([key, value]) => `${key}: ${value} ft`).join(", ");
+enum ControlOptions {
+    None,
+    Settings,
+    Initiative,
+    Hitpoints,
+    AC,
+    Conditions,
+    Spells,
+    Notes,
+    StatBlock
+}
+
+function renderSpeed(speed: Entity["StatBlock"]["Stats"]["Speed"]): JSX.Element {
+    return <div>
+        <strong>Speed:</strong><br />
+        {speed.Walk && <span><GiRun /> {speed.Walk} ft.</span>}
+        {speed.Fly && <span><GiLibertyWing /> {speed.Fly} ft.</span>}
+        {speed.Climb && <span><GiMountainClimbing /> {speed.Climb} ft.</span>}
+        {speed.Swim && <span><FaSwimmer /> {speed.Swim} ft.</span>}
+        {speed.Burrow && <span><GiDigDug /> {speed.Burrow} ft.</span>}
+    </div>
+}
+
+function renderSettingsControl(): JSX.Element {
+    return <div>
+        Settings - Not Implemented
+    </div>
+}
+
+function renderInitiativeControl(): JSX.Element {
+    return <div>
+        Initiative - Not Implemented
+    </div>
+}
+
+function renderHitpointsControl(): JSX.Element {
+    return <div>
+        Hitpoints - Not Implemented
+    </div>
+}
+
+function renderACControl(): JSX.Element {
+    return <div>
+        AC - Not Implemented
+    </div>
+}
+
+function renderConditionsControl(): JSX.Element {
+    return <div>
+        Conditions - Not Implemented
+    </div>
+}
+
+function renderSpellsControl(): JSX.Element {
+    return <div>
+        Spells - Not Implemented
+    </div>
+}
+
+function renderNotesControl(): JSX.Element {
+    return <div>
+        Notes - Not Implemented
+    </div>
+}
+
+function renderStatBlockControl(): JSX.Element {
+    return <div>
+        StatBlock - Not Implemented
+    </div>
 }
 
 export function EntityDisplay({ entity, expanded }: EntityDisplayProps) {
-    const [ExpandedState, SetExpandedState] = React.useState(expanded);
+    const [ExpandedState, SetExpandedState] = React.useState<boolean>(expanded || false);
+    const [ControlState, SetControlState] = React.useState<ControlOptions>(ControlOptions.None);
+
+    const FlipControlState = (state: ControlOptions) => {
+        if (ControlState === state) {
+            SetControlState(ControlOptions.None);
+        } else {
+            SetControlState(state);
+        }
+    }
 
     const dynamicStyle: React.CSSProperties = {
         columnCount: 2,
     };
 
     return (
-        <div>
-            <div className="displayCard entity" onClick={_ => { SetExpandedState(!ExpandedState) }}>
+        <div className="entity">
+            <div className="displayCard" onClick={_ => { SetExpandedState(!ExpandedState) }}>
                 {ExpandedState ?
                     <Card className="expanded" style={dynamicStyle}>
-                        <h4>{entity.IsHostile ? <img src={HostileIcon} alt="Hostile" /> : null} {entity.Name}</h4>
+                        <h4>{entity.IsHostile ? <GiCrossedSwords /> : null} {entity.Name}</h4>
                         <strong>Hit Points:</strong> {entity.CurrentHitPoints} / {entity.StatBlock.Stats.HitPoints.Average}<br />
                         <strong>Armor Class:</strong> {entity.StatBlock.Stats.ArmorClass}<br />
                         <strong>Initiative:</strong> {entity.Initiative}<br />
-                        <strong>Speed:</strong> {renderSpeed(entity.StatBlock.Stats.Speed)}<br />
+                        {renderSpeed(entity.StatBlock.Stats.Speed)}
                     </Card>
                     :
                     <Card className="collapsed">
-                        <span className="h4">{entity.IsHostile ? <img src={HostileIcon} alt="Hostile" /> : null} {entity.Name}</span>
-                        <span><strong>HP:</strong> {entity.CurrentHitPoints} / {entity.StatBlock.Stats.HitPoints.Average}</span>
-                        <span><strong>AC:</strong> {entity.StatBlock.Stats.ArmorClass}</span>
+                        <span className="h4">{entity.IsHostile ? <GiCrossedSwords /> : null} {entity.Name}</span>
+                        <p>
+                            <span><strong>HP:</strong> {entity.CurrentHitPoints} / {entity.StatBlock.Stats.HitPoints.Average}</span>
+                            <span><strong>AC:</strong> {entity.StatBlock.Stats.ArmorClass}</span>
+                        </p>
                     </Card>}
             </div>
             <div className="displayCardControls">
-                
+                <div className="space-evenly">
+                    <button onClick={_ => FlipControlState(ControlOptions.Settings)}><GiCog /></button>
+                    <button onClick={_ => FlipControlState(ControlOptions.Initiative)}><IoMdHourglass /></button>
+                    <button onClick={_ => FlipControlState(ControlOptions.Hitpoints)}><GiHalfHeart /></button>
+                    <button onClick={_ => FlipControlState(ControlOptions.AC)}><GiShield /></button>
+                    <button onClick={_ => FlipControlState(ControlOptions.Conditions)}><GiChalkOutlineMurder /></button>
+                    <button onClick={_ => FlipControlState(ControlOptions.Spells)}><GiSpellBook /></button>
+                    <button onClick={_ => FlipControlState(ControlOptions.Notes)}><GiPencil /></button>
+                    <button onClick={_ => FlipControlState(ControlOptions.StatBlock)}><FaAddressCard /></button>
+                </div>
+                {ControlState === ControlOptions.Settings ? renderSettingsControl() : null}
+                {ControlState === ControlOptions.Initiative ? renderInitiativeControl() : null}
+                {ControlState === ControlOptions.Hitpoints ? renderHitpointsControl() : null}
+                {ControlState === ControlOptions.AC ? renderACControl() : null}
+                {ControlState === ControlOptions.Conditions ? renderConditionsControl() : null}
+                {ControlState === ControlOptions.Spells ? renderSpellsControl() : null}
+                {ControlState === ControlOptions.Notes ? renderNotesControl() : null}
+                {ControlState === ControlOptions.StatBlock ? renderStatBlockControl() : null}
             </div>
         </div>
     );
--- src/routes/library.tsx
+++ src/routes/library.tsx
@@ -3,6 +3,8 @@ import { EntityDisplay } from "@src/components/entityDisplay"
 import { StatBlockEntity } from "@src/models/statBlockEntity"
 
 import { arasta } from "@src/temp/arasta"
+import { aurelia } from "@src/temp/aurelia"
+import { winter_ghoul } from "@src/temp/winter-ghoul"
 
 export const Route = createFileRoute("/library")({
     component: Library,
@@ -17,6 +19,8 @@ function Library() {
             </p>
 
             <EntityDisplay entity={new StatBlockEntity(arasta, 5)} expanded = {true}/>
+            <EntityDisplay entity={new StatBlockEntity(aurelia, 5)}/>
+            <EntityDisplay entity={new StatBlockEntity(winter_ghoul, 5)}/>
         </>
     )
 }
